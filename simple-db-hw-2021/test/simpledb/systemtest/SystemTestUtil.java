63b12ec85720d0ef0ba89f1c18d755207b2c7ff9
ziyu <1194981710@qq.com>
2023-10-26 09:29:48 +0800

    modify random seed

--- simple-db-hw-2021/test/simpledb/systemtest/SystemTestUtil.java
+++ simple-db-hw-2021/test/simpledb/systemtest/SystemTestUtil.java
@@ -66,7 +66,7 @@ public class SystemTestUtil {
             tuples = new ArrayList<>(rows);
         }
 
-        Random r = new Random(3322);
+        Random r = new Random();
 
         // Fill the tuples list with generated values
         for (int i = 0; i < rows; ++i) {
